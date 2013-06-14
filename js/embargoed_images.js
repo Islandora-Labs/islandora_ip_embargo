924ba7de654c7ecd92aa35bf4e4eb7d24012168a
William Panting <willtp87@hotmail.com>
2013-06-14 09:59:19 -0300

    lower font size

--- js/embargoed_images.js
+++ js/embargoed_images.js
@@ -2,5 +2,5 @@
 jQuery(document).ready(function($) {
   $('.islandora_ip_embargo_embargoed').wrap('<div style="position:relative;"></div>');
   $('.islandora_ip_embargo_embargoed').after('<span style="position:absolute;' +
-    'left:0em;text-decoration:none;color:red;font-size:2em">EMBARGOED</span>');
+    'left:0em;text-decoration:none;color:red;font-size:1.5em">EMBARGOED</span>');
 });
