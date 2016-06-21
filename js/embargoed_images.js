20c89ab3a2b40d93e11f0f3d673ab9ad06ca557f
brandonmorr <bmorrissey95@gmail.com>
2016-06-21 14:51:34 -0300

    Added 2 fields to settings UI, text and color are now customizable options for embargoed items

--- js/embargoed_images.js
+++ js/embargoed_images.js
@@ -1,6 +1,22 @@
-// Make embargoed thumbnails obvious.
-jQuery(document).ready(function($) {
-  $('.islandora_ip_embargo_embargoed').wrap('<div style="position:relative;"></div>');
-  $('.islandora_ip_embargo_embargoed').after('<span class="islandora_ip_embargo_embargoed" style="position:absolute;' +
-    'left:0em;text-decoration:none;color:red;font-size:1.5em">EMBARGOED</span>');
-});
+/**
+ * @file
+ * Show the overlay text for embargoed items.
+ */
+(function ($, Drupal, window, document, undefined) {
+Drupal.behaviors.islandora_ip_embargo = {
+  attach: function(context, settings) {
+    jQuery(document).ready(function($) {
+      $('.islandora_ip_embargo_embargoed').wrap('<div style="position:relative;"></div>');
+      $('.islandora_ip_embargo_embargoed').after('<span class="islandora_ip_embargo_embargoed"></span>');
+      $('.islandora_ip_embargo_embargoed').text(Drupal.t(Drupal.settings.islandora_ip_embargo.text));
+      $('span.islandora_ip_embargo_embargoed').css({
+        'color' : Drupal.t(Drupal.settings.islandora_ip_embargo.color),
+        'left' : '0',
+        'padding' : '0.5em',
+        'font-size' : '1.5em',
+        'position' : 'absolute',
+        'text-decoration' : 'none',
+      });
+    });
+  }};
+})(jQuery, Drupal, this, this.document);
