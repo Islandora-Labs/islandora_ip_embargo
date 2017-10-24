69a30d2b94746210883ac6b59b6a4d52519dc6da
qadan <daitken@discoverygarden.ca>
2017-10-24 13:54:01 -0300

    no, let's not do that

--- js/embargoed_images.js
+++ js/embargoed_images.js
@@ -5,18 +5,16 @@
 (function ($, Drupal, window, document, undefined) {
 Drupal.behaviors.islandora_ip_embargo = {
   attach: function(context, settings) {
-    $('.islandora_ip_embargo_embargoed').once('islandora-ip-embargo-overlay', function() {
-      $('.islandora_ip_embargo_embargoed').wrap('<div style="position:relative;"></div>');
-      $('.islandora_ip_embargo_embargoed').after('<span class="islandora_ip_embargo_embargoed"></span>');
-      $('.islandora_ip_embargo_embargoed').text(Drupal.t(Drupal.settings.islandora_ip_embargo.text));
-      $('span.islandora_ip_embargo_embargoed').css({
-        'color' : '#' + Drupal.settings.islandora_ip_embargo.color,
-        'left' : '0',
-        'padding' : '0.5em',
-        'font-size' : '1.25em',
-        'position' : 'absolute',
-        'text-decoration' : 'none',
-      });
+    $('.islandora_ip_embargo_embargoed').wrap('<div style="position:relative;"></div>');
+    $('.islandora_ip_embargo_embargoed').after('<span class="islandora_ip_embargo_embargoed"></span>');
+    $('span.islandora_ip_embargo_embargoed').text(Drupal.t(Drupal.settings.islandora_ip_embargo.text));
+    $('span.islandora_ip_embargo_embargoed').css({
+      'color' : '#' + Drupal.settings.islandora_ip_embargo.color,
+      'left' : '0',
+      'padding' : '0.5em',
+      'font-size' : '1.25em',
+      'position' : 'absolute',
+      'text-decoration' : 'none',
     });
   }};
 })(jQuery, Drupal, this, this.document);
